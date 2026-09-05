db4bad2075f73ec217c347edb5b542d419dad164
Augustin Blanchet <32638090+augstb@users.noreply.github.com>
2026-09-05 23:48:36 +0200

    Bump version to 1.1.1

--- src/main/java/fr/stillcraft/backyardban/core/BackyardBanConfig.java
+++ src/main/java/fr/stillcraft/backyardban/core/BackyardBanConfig.java
@@ -12,7 +12,7 @@ import java.util.function.Consumer;
  */
 public final class BackyardBanConfig {
     // Version (don't forget to increment)
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.1.1";
 
     // Used config files keys
     private static final String[] locale_keys = {
